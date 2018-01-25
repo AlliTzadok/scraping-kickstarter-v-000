c9d9fbd4e5f81534d3c4e2d9ed06173fe9a1c284
Alli Tzadok <allisontzadok@gmail.com>
2018-01-25 03:56:11 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,9 +6,10 @@ require 'nokogiri'
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
-  
+
   kickstarter = Nokogiri::HTML(html)
-end
+
 binding.pry
+end
 
 create_project_hash
