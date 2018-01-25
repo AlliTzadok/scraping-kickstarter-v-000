23f95562c8c2a89e5c0794bcd59c94f0bd531ddf
Alli Tzadok <allisontzadok@gmail.com>
2018-01-25 04:15:05 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -16,6 +16,9 @@ def create_project_hash
 
   kickstarter = Nokogiri::HTML(html)
   projects = {}
+
+  kickstarter.css("li.project.grid_4").each do |project|
+    project[project] = {}
 end
 
 create_project_hash
